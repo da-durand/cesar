01968239c40643da33cc65330ac36469bf1b8a01
dadurand <ha.halo@live.fr>
2019-09-26 16:59:45 +0200

    essais

--- cesar.py
+++ cesar.py
@@ -1,5 +1,25 @@
-word = "abd"
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j","k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+word = "abc"
 word2 = "nez"
 code3 = "hello"
 
-cryptage = ""
+encrypted_word = ""
+
+for i in range (len(word)):
+    char = word[i]
+    print(char)
+    encrypted_word += alphabet[i + 5]
+
+print(encrypted_word + "\n")
+
+encrypted_word = ""
+
+for i in range (len(word2)):
+    char = word2[i]
+    print(char)
+    encrypted_word += alphabet[i + 5]
+
+print(encrypted_word)
+
+encrypted_word = ""
